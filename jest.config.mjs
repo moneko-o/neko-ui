b32bb2b757f1c575a92906c3b3a7bcf5e066de2e
Moneko <meow.moneko@gmail.com>
2026-02-26 14:45:35 +0800

    更新 jest.config.mjs

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,7 +1,13 @@
+import { getUtil } from '@moneko/solid/jest';
+
 const config = {
   preset: '@moneko/solid',
   setupFilesAfterEnv: [
     '<rootDir>/test/setup.ts',
+    getUtil('css-highlights.js'),
+    getUtil('canvas.js'),
+    getUtil('event.js'),
+    getUtil('structured-clone.js'),
     '<rootDir>/test/install.ts',
   ],
 };
