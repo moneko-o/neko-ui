766a70b7c3bf0bbb8e937bc46514e6a527b15008
Moneko <meow.moneko@gmail.com>
2026-02-26 14:11:14 +0800

    更新 jest.config.mjs

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,19 +2,8 @@ const config = {
   preset: '@moneko/solid',
   setupFilesAfterEnv: [
     '<rootDir>/test/setup.ts',
-    '<rootDir>/test/css-highlights.ts',
-    '<rootDir>/test/canvas.ts',
-    '<rootDir>/test/event.ts',
-    '<rootDir>/test/structured-clone.ts',
     '<rootDir>/test/install.ts',
   ],
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/test/file.mock.ts',
-    '\\.(css|less|sass|scss)$': '<rootDir>/test/obj-proxy.ts',
-    '\\?raw$': '<rootDir>/test/file.mock.ts',
-    '\\?url$': '<rootDir>/test/file.mock.ts',
-  },
 };
 
 export default config;
