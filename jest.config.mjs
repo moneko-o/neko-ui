ac56c255f16a5c0b6d2b415b76f84daeb25e7f89
Moneko <poi.nyaa@qq.com>
2026-02-26 13:21:31 +0800

    更新 jest.config.mjs (#20)

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,22 +1,22 @@
-const ignorePatterns = [
-  '<rootDir>/test/',
-  '<rootDir>/lib/',
-  '<rootDir>/es/',
-  '<rootDir>/docs/',
-  '<rootDir>/node_modules/',
-];
+//const ignorePatterns = [
+//  '<rootDir>/test/',
+//  '<rootDir>/lib/',
+//  '<rootDir>/es/',
+//  '<rootDir>/docs/',
+//  '<rootDir>/node_modules/',
+//];
 
 const config = {
-  preset: '@moneko/core/solid-jest',
-  coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
-  testEnvironment: 'jsdom',
-  roots: ['components'],
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  testMatch: ['<rootDir>/components/**/__tests__/**/*.{js,jsx,ts,tsx}'],
-  transformIgnorePatterns: ['<rootDir>/lib/', '<rootDir>/es/', '<rootDir>/docs/'],
-  coveragePathIgnorePatterns: ignorePatterns,
-  testPathIgnorePatterns: ignorePatterns,
+  preset: '@moneko/solid',
+  //coverageDirectory: 'coverage',
+  //coverageProvider: 'v8',
+  //testEnvironment: 'jsdom',
+  //roots: ['components'],
+  //moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  //testMatch: ['<rootDir>/components/**/__tests__/**/*.{js,jsx,ts,tsx}'],
+  //transformIgnorePatterns: ['<rootDir>/lib/', '<rootDir>/es/', '<rootDir>/docs/'],
+  //coveragePathIgnorePatterns: ignorePatterns,
+  //testPathIgnorePatterns: ignorePatterns,
   setupFilesAfterEnv: [
     '<rootDir>/test/setup.ts',
     '<rootDir>/test/css-highlights.ts',
@@ -26,17 +26,17 @@ const config = {
     '<rootDir>/test/install.ts',
   ],
   moduleNameMapper: {
-    'neko-ui': '<rootDir>/components/index.ts',
+   // 'neko-ui': '<rootDir>/components/index.ts',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/test/file.mock.ts',
     '\\.(css|less)$': '<rootDir>/test/obj-proxy.ts',
     '\\?raw$': '<rootDir>/test/file.mock.ts',
     '\\?url$': '<rootDir>/test/file.mock.ts',
   },
-  transform: {
-    // '^.+\\.[jt]sx?$': '<rootDir>/test/transform-solid.mjs',
-    // '^.+\\.[jt]sx?$': '@moneko/core/solid-jest',
-  },
+  //transform: {
+  // '^.+\\.[jt]sx?$': '<rootDir>/test/transform-solid.mjs',
+  // '^.+\\.[jt]sx?$': '@moneko/core/solid-jest',
+  //},
 };
 
 export default config;
