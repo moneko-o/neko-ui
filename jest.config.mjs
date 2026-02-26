ff4695ec5086842d98641d0a116a8b3d426183c0
Moneko <meow.moneko@gmail.com>
2026-02-26 13:37:24 +0800

    更新 jest.config.mjs

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,22 +1,5 @@
-//const ignorePatterns = [
-//  '<rootDir>/test/',
-//  '<rootDir>/lib/',
-//  '<rootDir>/es/',
-//  '<rootDir>/docs/',
-//  '<rootDir>/node_modules/',
-//];
-
 const config = {
   preset: '@moneko/solid',
-  //coverageDirectory: 'coverage',
-  //coverageProvider: 'v8',
-  //testEnvironment: 'jsdom',
-  //roots: ['components'],
-  //moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  //testMatch: ['<rootDir>/components/**/__tests__/**/*.{js,jsx,ts,tsx}'],
-  //transformIgnorePatterns: ['<rootDir>/lib/', '<rootDir>/es/', '<rootDir>/docs/'],
-  //coveragePathIgnorePatterns: ignorePatterns,
-  //testPathIgnorePatterns: ignorePatterns,
   setupFilesAfterEnv: [
     '<rootDir>/test/setup.ts',
     '<rootDir>/test/css-highlights.ts',
@@ -26,17 +9,12 @@ const config = {
     '<rootDir>/test/install.ts',
   ],
   moduleNameMapper: {
-   // 'neko-ui': '<rootDir>/components/index.ts',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/test/file.mock.ts',
-    '\\.(css|less)$': '<rootDir>/test/obj-proxy.ts',
+    '\\.(css|less|sass|scss)$': '<rootDir>/test/obj-proxy.ts',
     '\\?raw$': '<rootDir>/test/file.mock.ts',
     '\\?url$': '<rootDir>/test/file.mock.ts',
   },
-  //transform: {
-  // '^.+\\.[jt]sx?$': '<rootDir>/test/transform-solid.mjs',
-  // '^.+\\.[jt]sx?$': '@moneko/core/solid-jest',
-  //},
 };
 
 export default config;
